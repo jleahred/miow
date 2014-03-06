7a3ff3e796ead12cd249f9b5c7f191f1f7874e7f
jlesteban <jlesteban>
2014-03-06 20:00:09 +0100

    minor modif

--- core/MqEditFind.py
+++ core/MqEditFind.py
@@ -24,16 +24,18 @@ class WithFind(QPlainTextEdit):
             QWidget.__init__(self, edit)
 
             self.edit = edit
-            self.adjustHeight()
+            self.find1 = QLineEdit(self)
+
 
         def paintEvent(self, event):
-            self.edit.find_line_paint(self, event)
+            self.find_line_paint(self, event)
             QWidget.paintEvent(self, event)
 
         def adjustHeight(self):
             height = self.fontMetrics().height()
             if self.height() != height:
                 self.setFixedHeight(height)
+                #self.find1.setFixedHeight(height)
             return self.height()
 
         def updateContents(self, rect, scroll):
@@ -42,30 +44,29 @@ class WithFind(QPlainTextEdit):
             else:
                 self.update()
 
+        def find_line_paint(self, number_bar, event):
+            painter = QPainter(number_bar)
+            painter.fillRect(event.rect(), QColor(230, 230, 255))
+            painter.end()
+
     def __init__(self, *args):
         self.find_line = self.FindLine(self)
         self.setFrameStyle(QFrame.NoFrame)
         self.updateRequest.connect(self.find_line.updateContents)
         self.__adjust_height()
-        self.find1 = QLineEdit(self)
 
     def __adjust_height(self):
-        height = self.find_line.adjustHeight()
+        #height = self.find_line.adjustHeight()
+        #height = self.find_line.find1.height()
         super(WithFind, self).set_viewport_margins("WithFind", 
-                                (0, 0, 0, height))
+                                (0, 0, 0, self.find_line.find1.height()-4))
 
-
-    def find_line_paint(self, number_bar, event):
-        painter = QPainter(number_bar)
-        painter.fillRect(event.rect(), QColor(230, 230, 230))
-        painter.end()
 
     def resizeEvent(self, resize_event):
         super(WithFind, self).resizeEvent(resize_event)
         self.find_line.setGeometry(QRect(0, 
-                                       self.height()-self.find_line.height(), 
-                                       self.width(), self.viewport().height()))
-        self.find1.setGeometry(QRect(8, self.height()- self.find_line.height()-1, 150, self.find_line.height()+3))
+                                       self.height()-self.find_line.find1.height(), 
+                                       self.width(), self.find_line.find1.height()))
 
 
 
